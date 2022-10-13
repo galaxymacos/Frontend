5b04b628f9e03194859b5764423f8bb971c852cb
xunruan <xunruan@icloud.com>
2022-10-13 13:04:20 -0400

    Get a more flexible structure

--- Vue Udemy/cmp-adv-01-starting-setup/src/App.vue
+++ Vue Udemy/cmp-adv-01-starting-setup/src/App.vue
@@ -3,12 +3,14 @@
     <the-header></the-header>
     <button @click="setSelectedComponent('active-goals')">Active Goals</button>
     <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
-    <component :is="selectedComponent"></component>
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import TheHeader from "@/components/TheHeader.vue";
+import TheHeader from "@/components/layout/TheHeader.vue";
 import ActiveGoals from "@/components/ActiveGoals.vue";
 import ManageGoals from "@/components/ManageGoals.vue";
 export default {
