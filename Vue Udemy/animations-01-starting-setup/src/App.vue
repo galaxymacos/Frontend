ab48eba6a0282d44c3cd8f5b5014d499dc56cae2
xunruan <xunruan@icloud.com>
2022-10-18 09:13:46 -0700

    use animation instead of transition to watch transform value change

--- Vue Udemy/animations-01-starting-setup/src/App.vue
+++ Vue Udemy/animations-01-starting-setup/src/App.vue
@@ -63,7 +63,6 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
-  transition: transform 0.3s ease-out;  /* watch the property "transform", making the change of the transform happen slowly */
 }
 .container {
   max-width: 40rem;
@@ -77,6 +76,20 @@ button:active {
   border-radius: 12px;
 }
 .animate {
-  transform: translateX(-150px);
+  animation: slide-left 0.3s ease-out forwards; /* change to the state value in a regulated duration */
+}
+
+@keyframes slide-left { /* set up some states */
+  0% {
+    transform: translateX(0) scale(1);
+  }
+
+  70% {
+    transform: translateX(-120px) scale(1.1);
+  }
+
+  100% {
+    transform: translateX(-150px) scale(1);
+  }
 }
 </style>
