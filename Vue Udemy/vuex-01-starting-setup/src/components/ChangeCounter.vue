84eb575d2e3e6aa5ac4acb40d62622afcb9fa4e2
xunruan <xunruan@icloud.com>
2022-10-19 20:48:10 -0700

    Vuex state, mutations & getters

---
+++ Vue Udemy/vuex-01-starting-setup/src/components/ChangeCounter.vue
@@ -0,0 +1,18 @@
+<template>
+<button @click='addOne'>Add one</button>
+</template>
+
+<script>
+export default {
+  name: 'ChangeCounter',
+  methods: {
+    addOne(){
+      this.$store.commit('increment')
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ Vue Udemy/vuex-01-starting-setup/src/components/TheCounter.vue
@@ -0,0 +1,18 @@
+<template>
+<h3>{{ counter }}</h3>
+</template>
+
+<script>
+export default {
+  name: 'TheCounter',
+  computed: {
+    counter() {
+      return this.$store.getters.finalCounter;
+    },
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
